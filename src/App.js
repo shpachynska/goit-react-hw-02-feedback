1b8fc547cf1fcbc3b2f9dbbef3c35e6ebc673411
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2021-10-01 20:31:09 +0200

    updates upon mentor comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import React, { Component } from 'react';
-import Feedback from './components/Feedback/Feedback';
+import styles from './components/Feedback/Feedback.module.css';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Statistics from './components/Statistics/Statistics';
+import Section from './components/Section/Section';
+import Notification from './components/Notification/Notification';
 
 class App extends Component {
   state = {
@@ -9,13 +13,47 @@ class App extends Component {
     bad: 0,
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
+  options = Object.keys(this.state);
 
+  handleIncrement = value => {
+    this.setState(prevState => ({
+      [value]: prevState[value] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
     return (
       <>
         <div className="App">
-          <Feedback defaultProps={(good, neutral, bad)} />
+          <div className={styles.container}>
+            <Section title="Please leave feedback">
+              <FeedbackOptions
+                options={this.options}
+                onLeaveFeedback={this.handleIncrement}
+              />
+            </Section>
+
+            <Section title="Statistics">
+              {this.countTotalFeedback() > 0 ? (
+                <Statistics
+                  values={this.state}
+                  totalCount={this.countTotalFeedback()}
+                  percentage={this.countPositiveFeedbackPercentage()}
+                />
+              ) : (
+                <Notification message="No feedback given" />
+              )}
+            </Section>
+          </div>
+          {/* <Feedback defaultProps={(good, neutral, bad)} /> */}
         </div>
       </>
     );
